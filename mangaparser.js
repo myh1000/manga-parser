52627634f8a54fc06f7e2ce5240f676a13ec0e2b
myh1000 <myh1000@gmail.com>
2017-03-28 19:04:09 -0700

    smol fix

--- mangaparser.js
+++ mangaparser.js
@@ -2,18 +2,20 @@
 mangaparser.js -- parses information from chapter titles
 */
 function parse(string) {
-    var result
+    var result = string
 
-    if (result == string.match(/(\W+?)(chapter|chap|ch)\.*(\s*[\d\.]*)/i)) {
+    if (result = string.match(/(\W+?)(chapter|chap|ch)\.*(\s*[\d\.]*)/i)) {
         if (parseFloat(result[3])) {
-            if (result[3].match(/\.+(\d+)/g))
+            if (result[3].match(/\.+(\d+)/g)) {
                 return parseFloat(result[3].replace(result[3].match(/\.+(\d+)/g), "." + (parseFloat(result[3].match(/\.+(\d+)/)[1]))));
-            else
+            }
+            else {
                 return parseFloat(result[3]);
+            }
         }
     }
 
-    if (result == string.match(/(\d+(\.\d+)?)/g)) {
+    if (result = string.match(/(\d+(\.\d+)?)/g)) {
         if (result[0].match(/\.+(\d+)/g)) {
             return parseFloat(result[0].replace(result[0].match(/\.+(\d+)/g), "." + (parseFloat(result[0].match(/\.+(\d+)/)[1]))));
         }
